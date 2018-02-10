2bd82ff8449ae56a9894c94c15042d6719e7e978
Anika <Anika@Computer.local>
2018-02-10 15:23:39 -0500

    we need hlep

--- public/js/indexPage.js
+++ public/js/indexPage.js
@@ -1,37 +1,37 @@
 $(document).ready(function(){
   //log in
-  function login(callback) {
-    var CLIENT_ID = 'a81cae611ae24eb0ab858bb3e1a7ebc3';
-    var REDIRECT_URI = 'http://localhost:8888/';
-    function getLoginURL(scopes) {
-        return 'https://accounts.spotify.com/authorize?client_id=' + CLIENT_ID +
-          '&redirect_uri=' + encodeURIComponent(REDIRECT_URI) +
-          '&scope=' + encodeURIComponent(scopes.join(' ')) +
-          '&response_type=token';
-    }
-
-    var url = getLoginURL([
-        'user-read-email'
-    ]);
-
-    var width = 450,
-        height = 730,
-        left = (screen.width / 2) - (width / 2),
-        top = (screen.height / 2) - (height / 2);
-
-    window.addEventListener("message", function(event) {
-        var hash = JSON.parse(event.data);
-        if (hash.type == 'access_token') {
-            callback(hash.access_token);
-        }
-    }, false);
-
-    var w = window.open(url,
-                        'Spotify',
-                        'menubar=no,location=no,resizable=no,scrollbars=no,status=no, width=' + width + ', height=' + height + ', top=' + top + ', left=' + left
-                       );
-
-}
+//   function login(callback) {
+//     var CLIENT_ID = 'a81cae611ae24eb0ab858bb3e1a7ebc3';
+//     var REDIRECT_URI = 'http://localhost:8888';
+//     function getLoginURL(scopes) {
+//         return 'https://accounts.spotify.com/authorize?client_id=' + CLIENT_ID +
+//           '&redirect_uri=' + encodeURIComponent(REDIRECT_URI) +
+//           '&scope=' + encodeURIComponent(scopes.join(' ')) +
+//           '&response_type=token';
+//     }
+//
+//     var url = getLoginURL([
+//         'user-read-email'
+//     ]);
+//
+//     var width = 450,
+//         height = 730,
+//         left = (screen.width / 2) - (width / 2),
+//         top = (screen.height / 2) - (height / 2);
+//
+//     window.addEventListener("message", function(event) {
+//         var hash = JSON.parse(event.data);
+//         if (hash.type == 'access_token') {
+//             callback(hash.access_token);
+//         }
+//     }, false);
+//
+//     var w = window.open(url,
+//                         'Spotify',
+//                         'menubar=no,location=no,resizable=no,scrollbars=no,status=no, width=' + width + ', height=' + height + ', top=' + top + ', left=' + left
+//                        );
+//
+// }
 
 function getSomething(accessToken) {
   return $.ajax ({
@@ -55,16 +55,24 @@ function getSomething(accessToken) {
         });
     }
 
+    function login() {
+      return $.ajax({
+        url: 'http://localhost:8888/login',
+        success(response) {
+          console.log(response);
+        }
+      });
+    }
+
 
   $("#getTracks").click(function(){
     // var spot = $.get https://api.spotify.com/v1/me/top/{tracks};
-    alert('hello you pressed me');
+    // alert('hello you pressed me');
 
-    login(function(accessToken) {
-      getUserData(accessToken).then(function(response) {
-                    console.log(response);
-                });
+    login().done(function(data){
+      console.log(data);
     });
+
 
   });
 });
--- server.js
+++ server.js
@@ -1,8 +1,51 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var request = require('request'); // "Request" library
+var querystring = require('querystring');
+var cookieParser = require('cookie-parser');
 
-app.use(express.static('public'))
-app.use('/static', express.static('public'))
+var client_id = 'a81cae611ae24eb0ab858bb3e1a7ebc3'; // Your client id
+var client_secret = 'a5423a514c194cd587893851467cb5ee'; // Your secret
+var redirect_uri = 'http://localhost:8888'; // Your redirect uri
+
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+var generateRandomString = function(length) {
+  var text = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+var stateKey = 'spotify_auth_state';
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'))
+   .use(cookieParser());
+
+
+app.get('/login', function(req, res) {
+
+  var state = generateRandomString(16);
+  res.cookie(stateKey, state);
+
+  // your application requests authorization
+  var scope = 'user-read-private user-read-email';
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: client_id,
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state
+    }));
+});
 
 // app.get('/', function(req, res) {
 //   res.send('index.html')
